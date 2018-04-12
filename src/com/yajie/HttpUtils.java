b72b4e3381857ca9a497958e20fbe3dc1f656484
sunyajienb <1838282005@qq.com>
2018-04-12 14:08:26 +0800

    Update HttpUtils.java
    http工具

--- src/com/yajie/HttpUtils.java
+++ src/com/yajie/HttpUtils.java
@@ -8,7 +8,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 public class HttpUtils {
-
+	// post发送text/plain请求
 	public static String postText(String url, String text) {
 		String result = "";
 
@@ -48,5 +48,96 @@ public class HttpUtils {
 
 		return result;
 	}
+	// post发送表单提交请求
+	public static String post(String url, Map<String, String> params, String charset, Map<String, String> headers)
+			throws IOException {
+		try {
+			List<NameValuePair> pairs = null;
+			if (params != null && !params.isEmpty()) {
+				pairs = new ArrayList<NameValuePair>(params.size());
+				for (Map.Entry<String, String> entry : params.entrySet()) {
+					String value = entry.getValue();
+					if (value != null) {
+						pairs.add(new BasicNameValuePair(entry.getKey(), value));
+					}
+				}
+			}
+			HttpPost httpPost = new HttpPost(url);
+			if (pairs != null && pairs.size() > 0) {
+				httpPost.setEntity(new UrlEncodedFormEntity(pairs, charset));
+			}
 
+			if (headers != null && headers.size() > 0) {
+				Set<Map.Entry<String, String>> entrys = headers.entrySet();
+				for (Map.Entry<String, String> entry : entrys) {
+					httpPost.addHeader(entry.getKey(), entry.getValue());
+				}
+			}
+
+			CloseableHttpClient httpClient;
+			RequestConfig config = RequestConfig.custom().setConnectTimeout(125000).setSocketTimeout(30000).build();
+			httpClient = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+			CloseableHttpResponse response = httpClient.execute(httpPost);
+			int statusCode = response.getStatusLine().getStatusCode();
+			if (statusCode != 200) {
+				httpPost.abort();
+				throw new RuntimeException("HttpClient,error status code :" + statusCode);
+			}
+			HttpEntity entity = response.getEntity();
+			String result = null;
+			if (entity != null) {
+				result = EntityUtils.toString(entity, charset);
+			}
+			EntityUtils.consume(entity);
+			response.close();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+
+	}
+	// get请求
+	public static String get(String url, String charset, Map<String, String> headers) throws Exception {
+		// get请求返回结果  
+		String strResult = "";
+		HttpGet request = null;
+
+		try {
+			CloseableHttpClient client = HttpClients.createDefault();
+			// 发送get请求  
+			request = new HttpGet(url);
+			// 设置请求和传输超时时间  
+			RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(125000).setConnectTimeout(30000)
+					.build();
+			request.setConfig(requestConfig);
+
+			if (headers != null && headers.size() > 0) {
+				Set<Map.Entry<String, String>> entrys = headers.entrySet();
+				for (Map.Entry<String, String> entry : entrys) {
+					request.addHeader(entry.getKey(), entry.getValue());
+				}
+			}
+
+			CloseableHttpResponse response = client.execute(request);
+			//请求发送成功，并得到响应  
+			int statusCode = response.getStatusLine().getStatusCode();
+			if (statusCode == HttpStatus.SC_OK) {
+				//读取服务器返回过来的json字符串数据  
+				HttpEntity entity = response.getEntity();
+				strResult = EntityUtils.toString(entity, charset);
+				return strResult;
+			} else {
+				request.abort();
+				throw new RuntimeException("HttpClient,error status code :" + statusCode);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (request != null) {
+				request.releaseConnection();
+			}
+		}
+	}
 }
