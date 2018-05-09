a853d7bd89219249eeee9f6b3244b4946b8642a7
sunyajienb <1838282005@qq.com>
2018-05-09 18:28:56 +0800

    Update FileUtils.java

--- src/com/yajie/FileUtils.java
+++ src/com/yajie/FileUtils.java
@@ -1,5 +1,10 @@
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,4 +41,63 @@ public class FileUtils {
 		return fileNames;
 	}
 
+	/**
+	 * @param fileName 通过FileReader读取
+	 * @return
+	 */
+	public static String readFile(String fileName) {
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			String line = null;
+			br = new BufferedReader(new FileReader(new File(fileName)));
+			while ((line = br.readLine()) != null) {
+				sb.append(line).append("\n");
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("读取文件时出错，" + e.getMessage(), e);
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return sb.toString();
+	}
+	
+	/**
+	 * @param fileName 通过FileInputStream读取
+	 * @return
+	 */
+	public static String readFileV2(String fileName) {
+		BufferedInputStream bis = null;
+		byte[] bytes = new byte[4096];
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			bis = new BufferedInputStream(new FileInputStream(new File(fileName)));
+			int n = 0;
+			while ((n = bis.read(bytes)) > 0) {
+				sb.append(new String(bytes, 0, n, "UTF-8"));
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("读取文件时出错，" + e.getMessage(), e);
+		} finally {
+			if (bis != null) {
+				try {
+					bis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return sb.toString();
+	}	
+	
 }
